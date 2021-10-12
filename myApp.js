59d441024aca19920058660e47a75d2a3ce383a4
Cesaru Desu <cesaru.desu@gmail.com>
2021-10-12 00:12:53 -0500

    add server

--- myApp.js
+++ myApp.js
@@ -1,5 +1,14 @@
-require('dotenv').config();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 
