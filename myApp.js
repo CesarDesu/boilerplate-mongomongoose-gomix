e0d1981e653814de41017e79f4a827e1d22a699e
Cesaru Desu <cesaru.desu@gmail.com>
2021-10-13 10:58:17 -0500

    p

--- myApp.js
+++ myApp.js
@@ -121,7 +121,7 @@ const queryChain = (done) => {
   Person.find({ favoriteFoods: foodToSearch })
     .sort({ name: 1 })
     .limit(2)
-    .select({ favoriteFoods: 0 })
+    .select({ favoriteFoods: 1, name: 1, age: 0 })
     .exec(function (err, data) {
       if (err) return console.log(err);
       done(null, data);
